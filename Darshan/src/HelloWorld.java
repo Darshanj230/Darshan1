429d0c939a3183461dde930a49caafcf67e8c5bd
Sandeep.D <sandeep.d@simplilearn.net>
2022-03-24 15:34:19 +0530

    Nihal

--- Darshan/src/HelloWorld.java
+++ Darshan/src/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 		System.out.println("Master branch"); 
 		System.out.println("Child branch_1");
 		System.out.println("Parent");
+		System.out.println("Child from ide");
 		
 		
 
